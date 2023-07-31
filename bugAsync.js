6e57072bb96619a90b57c915a7fcdba2dc8c9f99
Fernando Jarilla <fjarilla@gmail.com>
2023-07-31 23:38:58 +0200

    Solución al ejercicio 4

--- bugAsync.js
+++ bugAsync.js
@@ -2,7 +2,7 @@
     Ejercicio 4 Arreglar bug de asincronia
 
     Tenemos otro error que nuestro cliente nos pide arreglar. El cliente está pidiendo un usuario y nos dice que está usando el id correcto el 1. Pero que siempre le da undefined. Nos a pasado el código que tenemos que revisar y arreglar. Para este problema crear un archivo llamado bugAsync.js con la solución.
-*/
+
 
 // Este programa simula una llamada asincrónica para obtener un usuario
 function obtenerUsuario(id) {
@@ -19,5 +19,37 @@ function obtenerUsuario(id) {
 
 const usuario = obtenerUsuario(1);
 console.log(usuario);
+*/
 
-  // SOLUCION
+// SOLUCION
+
+/*
+El problema es que estamos trabajando con una función asincrónica (setTimeout()), por lo que el return usuario se ejecuta antes de que se termine de ejecutar el setTimeout. La solución es esperar a que se termine de ejecutar el setTimeout para retornar el usuario. Esto se puede hacer con callbacks o con promesas. En este caso, lo haremos con promesas.
+*/
+
+function obtenerUsuario(id) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (id === 1) {
+                resolve({ id: 1, nombre: 'John Doe' });
+            } else {
+                reject('El usuario no existe');
+            }
+        }, 2000);
+    });
+}
+
+/*
+Y ahora, para obtener el usuario podemos usar el método then() de las promesas, que se ejecuta cuando la promesa se resuelve correctamente, y el método catch() que se ejecuta cuando la promesa se rechaza, o el método async/await. Lo resolveré con promesas.
+*/
+
+const usuario1 = obtenerUsuario(1);
+usuario1
+    .then((usuario1) => console.log('Usuario1: ', usuario1))
+    .catch((error1) => console.log('Error1: ', error1));
+
+const usuario2 = obtenerUsuario(2);
+usuario2
+    .then((usuario2) => console.log('Usuario2: ', usuario2))
+    .catch((error2) => console.log('Error2: ', error2));
+
