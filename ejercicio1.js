2fff45dd16a1cc8b06c92f02cfb4018cc90cdbe9
Fernando Jarilla <fjarilla@gmail.com>
2023-08-01 09:59:02 +0200

    Solución al ejercicio 1

--- ejercicio1.js
+++ ejercicio1.js
@@ -5,3 +5,47 @@ Crea un archivo ejercicio1.js que tenga un objeto con los siguientes campos: Nom
 */
 
 // SOLUCION
+
+const student = {
+    name: 'Fernando',
+    surname: 'Jarilla',
+    bootcampTopics: [
+        'Git', 
+        'Fundamentos HTML, CSS3', 
+        'Introducción a JavaScript', 
+        'Modelado de datos e introducción a SQL', 
+        'Desarrollo backend con NodeJS', 
+        'Desarrollo frontend con JS',
+        'Frontend PRO',
+        'Web components',
+        'Fundamentos React',
+        'TDD con JS',
+        'Desarrollo backend avanzado con NodeJS',
+        'React avanzado',
+        'Configuración de servidores y despliegue de aplicaciones'
+    ],
+    jobSearch: false,
+    socialMedia: [
+        {
+            name: 'GitHub',
+            link: 'https://github.com/baiofer/'
+        }
+    ]
+};
+
+const showStudent = (student) => {
+    console.log('Estudiante: ');
+    console.log('   Nombre: ', student.name);
+    console.log('   Apellido: ', student.surname);
+    console.log('   Temas del bootcamp: ');
+    for (topic of student.bootcampTopics) {
+        console.log('      ', topic);
+    }
+    console.log('   En búsqueda activa de empleo: ', student.jobSearch ? 'Sí' : 'No');
+    console.log('   Redes sociales: ');
+    for (socialMedia of student.socialMedia) {
+        console.log('      ', socialMedia.name, ': ', socialMedia.link);
+    }
+}
+
+showStudent(student);
